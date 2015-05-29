beeb2de35d7470410c3dffa19b2ea37a7e3c4ded
Marian Lumba <mmlumba@gmail.com>
2015-05-29 09:21:27 -0400

    finish arrays assignment

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,13 @@
-module.exports.newArray = undefined;
+//array = [];
+module.exports.newArray = function(num1,num2,num3,num4){
+  return Array.prototype.slice.call(arguments);
+};
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function(inputArray){
+  for (var i = 0; i < Array.prototype.slice.call(arguments); i++ ){
+    inputArray.push();
+  }
+  var lastElementIndex = inputArray.length - 1;
+  var myArray = [inputArray[0], inputArray[lastElementIndex]];
+  return myArray;
+};

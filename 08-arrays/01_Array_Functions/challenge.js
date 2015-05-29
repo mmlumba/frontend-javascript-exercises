e9df94da663dc9c6ceba92d8949239121ec8bf0a
Marian Lumba <mmlumba@gmail.com>
2015-05-29 09:59:13 -0400

    finish array functions challenge

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,25 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(inputArray){
+  for (var i = 0; i < Array.prototype.slice.call(arguments); i++){
+    inputArray.push();
+  }
+  var myArray = inputArray.reverse();
+  myArray.unshift(1);
+  return myArray;
+};
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function(inputArray){
+  for (var i = 0; i < Array.prototype.slice.call(arguments); i++){
+    inputArray.push();
+  }
+  var myArray = inputArray.join('+');
+  myArray.toString();
+  return myArray;
+};
 
-module.exports.arrayQuantityPlusOne = undefined;
+module.exports.arrayQuantityPlusOne = function(inputArray){
+  for (var i = 0; i < Array.prototype.slice.call(arguments); i++){
+    inputArray.push();
+  }
+  var myPlusOne = inputArray.length + 1;
+  return myPlusOne;
+};

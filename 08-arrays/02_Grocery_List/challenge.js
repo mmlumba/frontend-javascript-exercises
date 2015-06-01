1f7b8ff48d66ebb22ac0f03b46593cb97d1d48ed
Marian Lumba <mmlumba@gmail.com>
2015-06-01 09:49:17 -0400

    finished reverseSortedList

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,18 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(addSomething, inputArray){ //fix
+  for (var i; i < inputArray.length; i++){
+    if (addSomething!=inputArray[i]){
+      inputArray.push(addSomething);
+    }
+    else {
+      return inputArray;
+    }
+  }
+};
 
-module.exports.reverseSortedList = undefined;
+module.exports.reverseSortedList = function(inputArray){
+  for (var i = 0; i < Array.prototype.slice.call(arguments); i++){
+    inputArray.push();
+  }
+  var sorted = inputArray.sort();
+  return sorted.reverse();
+};

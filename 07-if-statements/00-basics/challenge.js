f7f38257161cc2b3b9239766699c50f4a1723df5
Marian Lumba <mmlumba@gmail.com>
2015-06-01 09:50:25 -0400

    finished favoriteNumber and checkLock

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,5 +1,23 @@
-module.exports.favoriteNumber = undefined;
+module.exports.favoriteNumber = function(fav, guess){
+  if (fav > guess) {
+    return "Too low";
+  }
+  else if (fav < guess) {
+    return "Too high";
+  }
+  else {
+    return "You got it!";
+  }
+};
 
-module.exports.checkLock = undefined;
+module.exports.checkLock = function(num1, num2, num3, num4){
+  if ((num1 === 3 || 5 || 7) && (num2 == 2) && (num3 >= 5 && num3 <= 100) && (num4 < 9 && num4 > 20)){
+    return "correct";
+  }
+  else {
+    return "incorrect";
+  }
+};
 
-module.exports.canIGet = undefined;
+//module.exports.canIGet = undefined;
+// (num1 == 3 || 5 || 7) && (num2 == 2) && (num3 >= 5 && num3 <= 100) && (num4 < 9 && num4 > 20)
